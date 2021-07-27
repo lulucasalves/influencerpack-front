94be067dc5a72a7b1c0efee7f8208ce82ffa0542
Lucas Alves <lucas.alves.supus@outlook.com>
2021-07-27 11:16:14 -0300

    complete-site-1920

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
